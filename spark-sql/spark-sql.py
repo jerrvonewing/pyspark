ac7ca45fa996776e43608c1298adc1261c07965c
jerrvon <jerrvonewing.cs@gmail.com>
2023-09-13 18:34:05 -0500

    Spark SQL script that does a query on age groups, and groups by age

---
+++ spark-sql/spark-sql.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+
+# Create a SparkSession
+spark = SparkSession.builder.appName("SparkSQL").getOrCreate()
+
+# Function to map the fields from the file
+# return the respective fields as a collection
+def mapper(line):
+    fields = line.split(',')
+    return Row(ID=int(fields[0]), name=str(fields[1].encode("utf-8")), \
+               age=int(fields[2]), numFriends=int(fields[3]))
+
+# Data file
+file_name = "file:////Users/jerrvon/Documents/pyspark-projects/data-files/fakefriends.csv"
+
+# Map through each line in the file and save each line result
+lines = spark.sparkContext.textFile(file_name)
+people = lines.map(mapper)
+
+# Infer the schema, and register the DataFrame as a table.
+schemaPeople = spark.createDataFrame(people).cache()
+schemaPeople.createOrReplaceTempView("people")
+
+# SQL can be run over DataFrames that have been registered as a table.
+teenagers = spark.sql("SELECT * FROM people WHERE age >= 13 AND age <= 19")
+
+# The results of SQL queries are RDDs and support all the normal RDD operations.
+for teen in teenagers.collect():
+  print(teen)
+
+# We can also use functions instead of SQL queries:
+schemaPeople.groupBy("age").count().orderBy("age").show()
+
+spark.stop()
